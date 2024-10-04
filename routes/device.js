cf6f3eec048c1bb0384c68c6fca8508ee12e228f
ZDUC <tduczero178@gmail.com>
2024-10-04 15:48:03 +0700

    update

--- routes/device.js
+++ routes/device.js
@@ -5,4 +5,5 @@ const { updateData, getData, getDeviceByTime } = require('../controllers/device'
 router.put('/update-device/:id', updateData)
 router.get('/get-device', getData)
 router.get('/get-device/table_device', getDeviceByTime)
+
 module.exports = router
